6951130781dd48c1bbc636c7e2e42187f540aa5e
Christiaan baes <chris.baes@gmail.com>
2012-12-22 15:02:47 +0100

    Also added an easyhttp csharp project.

---
+++ NancyDemo.Csharp.Easyhttp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EasyHttp.Http;
+
+namespace NancyDemo.Csharp.Easyhttp
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var http = new HttpClient();
+            http.Request.Accept = HttpContentTypes.ApplicationJson;
+            var trees = http.Get("http://localhost:55360/trees");
+            foreach(var t in trees.DynamicBody.Trees)
+            {
+                Console.WriteLine(t.Id);
+                Console.WriteLine(t.Genus);
+            }
+            var result = http.Get("http://localhost:55360/trees/1");
+            var tree = result.DynamicBody;
+            Console.WriteLine(tree.Id);
+            Console.WriteLine(tree.Genus);
+            Console.ReadLine();
+        }
+    }
+}
