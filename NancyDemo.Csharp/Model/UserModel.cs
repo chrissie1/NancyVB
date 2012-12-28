6cee71ba03baf708f4092d0e71e106f0bb65a685
Christiaan baes <chris.baes@gmail.com>
2012-12-28 14:47:55 +0100

    Added imageuploading and showing

--- NancyDemo.Csharp/Model/UserModel.cs
+++ NancyDemo.Csharp/Model/UserModel.cs
@@ -10,6 +10,7 @@ namespace NancyDemo.Csharp.Model
         public IEnumerable<String> Claims { get; set; } 
         public String Password { get; set; }
         public String RealName { get; set; }
+        public String FileDetails { get; set; }
 
         public UserModel()
         {
--- NancyDemo.Csharp/Modules/UsersModule.cs
+++ NancyDemo.Csharp/Modules/UsersModule.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Nancy.ModelBinding;
 using NancyDemo.Csharp.Model;
 using Nancy;
 using Nancy.Security;
@@ -8,7 +11,7 @@ namespace NancyDemo.Csharp.Modules
 {
     public class UsersModule : NancyModule
     {
-        public UsersModule(UserService userService)
+        public UsersModule(UserService userService, IRootPathProvider pathProvider)
         {
             this.RequiresAuthentication();
             Get["/users"] = parameters => View[new UsersModel() {Users = userService.GetUsers()}];
@@ -25,6 +28,48 @@ namespace NancyDemo.Csharp.Modules
                     {
                         return HttpStatusCode.NotFound;
                     }
+                };
+            Get["/users/userimageupload/{Id}"] = parameters =>
+            {
+                Guid result;
+                var isGuid = Guid.TryParse(parameters.id, out result);
+                var user = userService.GetById(result);
+                if (isGuid && user != null)
+                {
+                    return View["UploadUserImage",user];
+                }
+                return HttpStatusCode.NotFound;
+            };
+            Post["/users/userimageupload/{Id}"] = parameters =>
+                {
+                    Guid result;
+                    var isGuid = Guid.TryParse(parameters.Id, out result);
+                    var user = userService.GetById(result);
+               
+                    var file = this.Request.Files.FirstOrDefault();
+
+                    if (isGuid && user != null && file != null)
+                    {
+                        var fileDetails = string.Format("{3} - {0} ({1}) {2}bytes", file.Name, file.ContentType, file.Value.Length, file.Key);
+                        user.FileDetails = fileDetails;
+                        var filename = Path.Combine(pathProvider.GetRootPath(), "Images", user.Id + ".jpeg");
+
+                        using (var fileStream = new FileStream(filename, FileMode.Create))
+                        {
+                            file.Value.CopyTo(fileStream);
+                        }
+                        return View[user];
+                    }
+                    return HttpStatusCode.NotFound;
+                };
+            Get["/users/getimage/{Id}"] = parameters =>
+                {
+                    Guid result;
+                    var isGuid = Guid.TryParse(parameters.Id, out result);
+                    var filename = Path.Combine(pathProvider.GetRootPath(), "Images", result.ToString() + ".jpeg");
+                    if (!File.Exists(filename)) filename = Path.Combine(pathProvider.GetRootPath(), "Images", "emptyuser.jpeg");
+                    var stream = new FileStream(filename, FileMode.Open);
+                    return Response.FromStream(stream, "image/jpeg");
                 };
         }
     }
